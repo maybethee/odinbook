2a49ca33ab3c8534da68e24ba9c141ed3db9ddde
retra <jozalamun@gmail.com>
2024-06-03 09:48:45 -0400

    Create scope in post model to filter posts by those belonging to followed users

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,4 +4,9 @@ class Post < ApplicationRecord
   has_many :comments, dependent: :destroy
 
   validates :content, length: { maximum: 240 }
+
+  scope :from_users_followed_by, -> (user) do
+    followed_user_ids = "SELECT followed_id FROM relationships WHERE follower_id = :user_id"
+    where("user_id IN (#{followed_user_ids}) OR user_id = :user_id", user_id: user.id)
+  end
 end
